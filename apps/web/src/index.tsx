8219ce7b67dc291edae74e7af32b1c346dfe0c3a
sky <sky-devops@liongaming.co>
2024-03-26 17:17:02 +0800

    temp

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -8,7 +8,7 @@ import { LoginModule } from "@tsdaodao/login";
 import { DataSourceModule } from "@tsdaodao/datasource";
 import { ContactsModule } from "@tsdaodao/contacts";
 
-const apiURL = "https://api.botgate.cn/v1/";
+const apiURL = "http://192.168.10.105:8090/v1/";
 
 if ((window as any).__TAURI_IPC__) {
   // tauri环境
